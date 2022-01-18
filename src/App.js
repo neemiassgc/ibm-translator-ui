126803c440961d2901da5478cc5d8679cfa9fbd8
Neemias Santos <neemiassgc@outlook.com>
2022-01-18 20:21:38 -0300

    Add loaders to the buttons and improve overall design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BsTranslate } from 'react-icons/bs';
 import { MdSync } from 'react-icons/md';
-import mainButtonLoader from "./ball-triangle.svg";
-import swapButtonLoader from "./three-dots.svg";
+import ballTriangleLoader from "./ball-triangle.svg";
+import threeDotsLoader from "./three-dots.svg";
 import * as translator from "./services/translator";
 
 function Header() {
@@ -24,67 +24,64 @@ function Footer() {
   )
 }
 
-class Field extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      languageOptions: []
-    }
-  }
-
-  componentDidMount() {
-    this.fetchOptions()
-  }
-
-  fetchOptions() {
-    translator.fetchLanguages()
-      .then(data => {
-        this.setState({
-          languageOptions: data.map((lang, index) => (<option code={lang.code} key={index + 1} value={lang.name}>{lang.name}</option>))
-        })
-      })
-      .catch(console.log);
-  }
-
-  render() {
-    let button = null;
-    if (this.state.languageOptions.length > 0) {
-      button = (
-        <select code={this.props.args.code} value={this.props.args.option} className="bg-white text-zinc-700 text-xl text-center font-medium block my-auto p-3 w-full" onChange={this.props.onChangeLanguageOption}>
-          {this.state.languageOptions}
-        </select>
-      )
-    }
-    else button = (<img src={swapButtonLoader} className="mx-auto py-3" alt="Loading..."/>)
-
-    return (
-      <div className="w-full border-2 border-stone-200 rounded-md shadow-md">
-       <div className="border-b-2 border-stone-100 w-full">
-          {button}
-       </div>
-        <div className="p-2">
-          <textarea disabled={this.props.args.disabled} onChange={this.props.onChangeText} value={this.props.args.text} className="w-full h-44 bg-white outline-none p-4 text-zinc-700" style={{"resize": "none"}}></textarea>
-        </div>
-        <div className="p-1 text-right mx-2">
-          {this.props.args.text.length}/500
-        </div>
-      </div>
+function Field(props) {
+  let button = null;
+  if (props.status === "loading")
+    button = (<img src={threeDotsLoader} className="mx-auto py-3" alt="Loading..."/>)
+  else {
+    button = (
+      <select code={props.args.code} value={props.args.option} className="bg-white text-zinc-700 text-xl text-center font-medium block my-auto p-3 w-full" onChange={props.onChangeLanguageOption}>
+        {props.languageOptions}
+      </select>
     )
   }
-}
 
-function SwapButton(props) {
   return (
-    <button disabled={props.spinning} className="font-bold p-3 rounded-full ring-2 ring-indigo-600 bg-white active:bg-indigo-600 text-indigo-500 active:text-white hover:bg-indigo-200 h-14 w-14" onClick={props.onClick}>
-      <MdSync size="lg"/>
-    </button>
+    <div className="w-full border-2 border-stone-200 rounded-md shadow-md">
+      <div className="border-b-2 border-stone-100 w-full">
+        {button}
+      </div>
+      <div className="p-2">
+      <textarea disabled={props.status === "loading" || props.status === "translating"}
+        onChange={props.onChangeText}
+        value={props.args.text}
+        className="w-full h-44 bg-white outline-none p-4 text-zinc-700 no-underline"
+        style={{"resize": "none"}}
+        spellCheck={false}>
+      </textarea>
+      </div>
+      <div className="p-1 text-right mx-2">
+        {props.args.text.length}/500
+      </div>
+    </div>
   )
 }
 
-function MainButton(props) {
-  let component = null
+function SwapButton(props) {
+  let button = null
+  
+  if (props.status === "loading") 
+    button = (<img className="mx-auto" src={threeDotsLoader} alt="threeDotsLoader" />)
+  else {
+    button = (
+      <button disabled={props.status === "translating"}
+        className="font-bold p-3 rounded-full ring-2 ring-indigo-600 bg-white active:bg-indigo-600 text-indigo-500 active:text-white hover:bg-indigo-200 h-14 w-14"
+        onClick={props.onClick}>
+        <MdSync size="lg"/>
+      </button>
+    )
+  }
 
-  if (props.spinning) component = (<img className="mx-auto" src={mainButtonLoader} alt="mainButtonLoader" />)
+  return button
+}
+
+function MainButton(props) {
+  let button = null
+
+  if (props.status === "translating")
+    button = (<img className="mx-auto" src={ballTriangleLoader} alt="ballTriangleLoader" />)
+  else if (props.status === "loading")
+    button = (<img className="mx-auto" src={threeDotsLoader} alt="threeDotsLoader" />)
   else {
     const classes = [
       "bg-white",
@@ -102,7 +99,7 @@ function MainButton(props) {
       "rounded"
     ]
     
-    component =(
+    button =(
       <button className={classes.join(" ")} onClick={props.onClick}>
         Translate
       </button>
@@ -111,7 +108,7 @@ function MainButton(props) {
 
   return (
     <div className="mt-2 w-full text-center p-3 static">
-      {component}
+      {button}
     </div>
   )
 }
@@ -124,16 +121,30 @@ class App extends React.Component {
         option: "English",
         text: "",
         code: "en",
-        disabled: false
       },
       rightField: {
         option: "Português",
         text: "",
         code: "pt",
-        disabled: true
       },
-      spinning: false
+      status: "loading",
+      languageOptions: []
     }
+  }
+
+  componentDidMount() {
+    this.fetchOptions()
+  }
+
+  fetchOptions() {
+    translator.fetchLanguages()
+      .then(data => {
+        this.setState({
+          languageOptions: data.map((lang, index) => (<option code={lang.code} key={index + 1} value={lang.name}>{lang.name}</option>)),
+          status: "idle"
+        })
+      })
+      .catch(err => this.state.setState({languageOptions: err, status: "idle"}));
   }
   
   handleLanguageOption(label, event) {
@@ -226,12 +237,8 @@ class App extends React.Component {
     });
   }
 
-  toggleSpinner() {
-    this.setState( prevState => {
-      return {
-        spinning: !prevState.spinning
-      }
-    });
+  changeStatus(status) {
+    this.setState({status: status})
   }
   
   handleChangeText(label, event) {
@@ -244,7 +251,7 @@ class App extends React.Component {
   }
 
   translate() {
-    this.toggleSpinner()
+    this.changeStatus("translating")
 
     translator.translate(this.state.leftField.code, this.state.rightField.code, this.state.leftField.text)
       .then(data => {
@@ -253,16 +260,15 @@ class App extends React.Component {
             rightField: {...prevState.rightField, text: data},
           }
         })
-        this.toggleSpinner()
       })
       .catch(err => {
-        this.toggleSpinner()
         this.setState(prevState => {
           return {
             rightField: {...prevState.rightField, text: err.message},
           }
         })
       })
+      .finally(() => this.changeStatus("idle"))
   }
 
   render() {
@@ -272,19 +278,23 @@ class App extends React.Component {
         
         <div className="flex-cols md:flex w-full mt-5">
           <div className="p-10 flex-grow">
-            <Field args={this.state.leftField} onChangeLanguageOption={this.handleLanguageOption.bind(this, "left")} onChangeText={this.handleChangeText.bind(this, "left")}> </Field>
+            <Field args={{...this.state.leftField}} status={this.state.status} languageOptions={this.state.languageOptions}
+            onChangeLanguageOption={this.handleLanguageOption.bind(this, "left")}
+            onChangeText={this.handleChangeText.bind(this, "left")}> </Field>
           </div>
 
           <div className="flex justify-center md:items-center">
-            <SwapButton spinning={this.state.spinning} onClick={this.handleRotateLanguageOption.bind(this)} />
+            <SwapButton status={this.state.status} onClick={this.handleRotateLanguageOption.bind(this)} />
           </div>
 
           <div className="p-10 flex-grow">
-            <Field args={this.state.rightField} onChangeLanguageOption={this.handleLanguageOption.bind(this, "right")} onChangeText={this.handleChangeText.bind(this, "right")}> </Field>
+            <Field args={{...this.state.rightField}} status={this.state.status} languageOptions={this.state.languageOptions}
+            onChangeLanguageOption={this.handleLanguageOption.bind(this, "right")}
+            onChangeText={this.handleChangeText.bind(this, "right")}> </Field>
           </div>
         </div>
 
-        <MainButton spinning={this.state.spinning} onClick={this.translate.bind(this)}/>
+        <MainButton status={this.state.status} onClick={this.translate.bind(this)}/>
 
         <Footer />
       </div>
